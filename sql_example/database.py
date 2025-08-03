02d3987450f7f8ddbba065288d43a8f86936120f
Rustam Davletshin <rdavletshin@live.com>
2025-08-04 00:12:23 +0300

    sql_example. Understanding CRUD operations with SQLAlchemy

--- sql_example/database.py
+++ sql_example/database.py
@@ -1,10 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    Mapped,
-    mapped_column,
-    sessionmaker,
-)
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
 
 DATABASE_URL = "sqlite:///./test.db"
